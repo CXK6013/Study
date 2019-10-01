58789de0ba526cefe5c65600b2f3f07faebb78da
CXK6013 <1262372091@qq.com>
2019-10-01 22:11:30 +0800

    测试Git

---
+++ src/com/cxk/lambda/TestCom.java
@@ -0,0 +1,17 @@
+package com.cxk.lambda;
+
+import java.util.Arrays;
+
+/**
+ * @author XingKe
+ * @date 2019-04-27 11:34
+ */
+public class TestCom {
+    public static void main(String[] args) {
+        Person[] persons = new Person[2];
+        persons[0] = new Person(20,"张三");
+        persons[1] = new Person(30,"李四");
+        Arrays.sort(persons,(Person p1 ,Person p2)->Integer.compare(p1.getAge(),p2.getAge()));
+        System.out.println(Arrays.toString(persons));
+    }
+}

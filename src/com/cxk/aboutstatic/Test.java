21e2fa2bb9cfae6d51377b56669b5d72f6b01809
CXK6013 <1262372091@qq.com>
2019-10-06 16:05:14 +0800

    测试Git

---
+++ src/com/cxk/aboutstatic/Test.java
@@ -0,0 +1,24 @@
+package com.cxk.aboutstatic;
+
+/**
+ * @author XingKe
+ * @date 2019-04-26 21:41
+ */
+class SuperClass{
+    static {
+        System.out.println("这是父类");
+    }
+    public static  int a = 0;
+}
+class   ChildClass{
+    static {
+        System.out.println("这是子类");
+    }
+    public final static  int value = 0;
+}
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(SuperClass.a);
+        ChildClass[] ch = new ChildClass[10];
+    }
+}

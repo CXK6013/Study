f7dc6ab3338810b9241755dceb6ead724c552388
CXK6013 <1262372091@qq.com>
2019-10-06 16:06:27 +0800

    测试Git

---
+++ src/com/cxk/pojo/Student.java
@@ -0,0 +1,15 @@
+package com.cxk.pojo;
+
+/**
+ * @author XingKe
+ * @date 2019-06-08 12:59
+ */
+public class Student {
+
+    private String name = "zhangsan";
+    private int    age  = 20;
+
+    public Student() {
+        System.out.println(name+age);
+    }
+}
